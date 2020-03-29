98cf99fcaad204e7002ad516dcd9891ca268c636
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-03-29 12:49:31 +0200

    test book api

--- src/main/java/pl/coderslab/DriverApplication.java
+++ src/main/java/pl/coderslab/DriverApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScrumLabApplication {
+public class DriverApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ScrumLabApplication.class, args);
+        SpringApplication.run(DriverApplication.class, args);
     }
 }
---
+++ src/main/java/pl/coderslab/book/BookController.java
@@ -0,0 +1,30 @@
+package pl.coderslab.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController("/book")
+class BookController {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Book findById(@PathVariable ("id") Long id) {
+        return null;
+    }
+}
