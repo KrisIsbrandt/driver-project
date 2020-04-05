7f97bde967720d7aac2591b4707b61dd41171bc4
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-04-05 15:55:46 +0200

    Configuration: -Added ModelMapper bean for Entity to DTO conversion

--- src/main/java/pl/coderslab/DriverApplication.java
+++ src/main/java/pl/coderslab/DriverApplication.java
@@ -1,10 +1,14 @@
 package pl.coderslab;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
 import pl.coderslab.config.StorageProperties;
 import pl.coderslab.service.storage.StorageService;
 
@@ -16,10 +20,15 @@ public class DriverApplication {
     }
 
     @Bean
-    CommandLineRunner init(StorageService storageService) {
-        return (args) -> {
-            storageService.deleteAll();
-            storageService.init();
-        };
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
+
+//    @Bean
+//    CommandLineRunner init(StorageService storageService) {
+//        return (args) -> {
+//            storageService.deleteAll();
+//            storageService.init();
+//        };
+//    }
 }
