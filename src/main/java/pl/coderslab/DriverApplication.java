47f134d4aec60be82d5355cce039ad4ee441988c
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-04-08 19:42:05 +0200

    Config: -uncommented storage folder initialization on app start

--- src/main/java/pl/coderslab/DriverApplication.java
+++ src/main/java/pl/coderslab/DriverApplication.java
@@ -5,12 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
 import pl.coderslab.config.StorageProperties;
-import pl.coderslab.service.storage.StorageService;
+import pl.coderslab.service.storage.AssetService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -24,11 +21,11 @@ public class DriverApplication {
         return new ModelMapper();
     }
 
-//    @Bean
-//    CommandLineRunner init(StorageService storageService) {
-//        return (args) -> {
-//            storageService.deleteAll();
-//            storageService.init();
-//        };
-//    }
+    //Create storage folder on app start
+    @Bean
+    CommandLineRunner init(AssetService storageService) {
+        return (args) -> {
+            storageService.init();
+        };
+    }
 }
