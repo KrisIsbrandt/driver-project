f374b46421eb661ec700edc693b99b8b84a4d672
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-05-26 12:11:00 +0200

    Model: -Article implements Serializable interface

--- src/main/java/pl/coderslab/model/Article.java
+++ src/main/java/pl/coderslab/model/Article.java
@@ -1,6 +1,7 @@
 package pl.coderslab.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashSet;
@@ -8,7 +9,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "articles")
-public class Article {
+public class Article implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
