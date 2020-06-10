135c59180f854f9cc46a1c9e2bab263cf9e2737d
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-06-10 21:34:35 +0200

    misc: -remove unnecessary files

--- src/main/java/pl/coderslab/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.coderslab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.coderslab.model.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
